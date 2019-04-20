254dfa7c514c5be7ce24bdcee6f021d8e3644254
fakhrip <fakhriputra123s@gmail.com>
2019-04-20 12:01:09 +0700

    firstCommit

---
+++ readme.c
@@ -0,0 +1,35 @@
+include<stdio.h>
+
+int input;
+
+main(){
+
+  printf("          d8888b.  .d8b.  .d8888. db   dD  .d88b.  .88b  d88.                         \
+                    88  `8D d8' `8b 88'  YP 88 ,8P' .8P  Y8. 88'YbdP`88                         \
+                    88   88 88ooo88 `8bo.   88,8P   88    88 88  88  88                         \
+                    88   88 88~~~88   `Y8b. 88`8b   88    88 88  88  88                         \
+                    88  .8D 88   88 db   8D 88 `88. `8b  d8' 88  88  88                         \
+                    Y8888D' YP   YP `8888Y' YP   YD  `Y88P'  YP  YP  YP                         \
+                                                                                                \
+                                                                                                \
+d8888b. d88888b  .o88b. d8888b. db    db d888888b d888888b .88b  d88. d88888b d8b   db d888888b \
+88  `8D 88'     d8P  Y8 88  `8D 88    88   `88'   `~~88~~' 88'YbdP`88 88'     888o  88 `~~88~~' \
+88oobY' 88ooooo 8P      88oobY' 88    88    88       88    88  88  88 88ooooo 88V8o 88    88    \
+88`8b   88~~~~~ 8b      88`8b   88    88    88       88    88  88  88 88~~~~~ 88 V8o88    88    \
+88 `88. 88.     Y8b  d8 88 `88. 88b  d88   .88.      88    88  88  88 88.     88  V888    88    \
+88   YD Y88888P  `Y88P' 88   YD ~Y8888P' Y888888P    YP    YP  YP  YP Y88888P VP   V8P    YP    \n", );
+
+  printf("Choose menu: \n");
+  printf("1. getTheCode()\n");
+  printf("2. getTheStatus()\n");
+  scanf("%d\n", input);
+  switch (input) {
+    case 1:
+      printf("")
+      break;
+    case 2:
+      break;
+    default:
+      break;
+  }
+}
